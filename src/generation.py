cf3698ff7ef782e2b7cec3683b589071480e481d
mchung25 <102553221+mchung25@users.noreply.github.com>
2025-02-20 18:29:07 -0600

    [updated generation.py to follow query limits on r1]

--- src/generation.py
+++ src/generation.py
@@ -1,9 +1,11 @@
 import os
+import time
 import json
 import pandas as pd
 from together import Together
 import argparse
 from tqdm import tqdm
+from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_id", type=str, required=True, default="deepseek-ai/DeepSeek-V3")
@@ -12,9 +14,10 @@ parser.add_argument("--user_prompt_path", type=str, required=True)
 parser.add_argument("--output_path", type=str, required=True)
 args = parser.parse_args()
 
-api_key = "PASTE YOUR API KEY HERE"
-if api_key == "PASTE YOUR API KEY HERE":
-    raise ValueError("Please replace the default API key with your actual Together API key")
+load_dotenv()
+api_key = os.getenv("TOGETHERAI_API_KEY")
+if not api_key:
+    raise ValueError("TOGETHERAI_API_KEY not found in .env file")
 
 def generate_text(model_id,system_prompt, user_prompt):
     """Calls the TogetherAI API to generate text."""
@@ -55,7 +58,7 @@ if __name__ == "__main__":
     with open(user_prompt_path, "r") as f:
         user_prompt_list = f.readlines()
     
-    
+    time.sleep(50)
     
     generated_data = []
     total_iterations = len(sys_prompt_list) * len(user_prompt_list)
@@ -69,6 +72,8 @@ if __name__ == "__main__":
                     "answer": answer
                 })
                 pbar.update(1)
+                print(answer)
+                time.sleep(50)
 
     with open(output_path, "w") as f:
         json.dump(generated_data, f, indent=4)
