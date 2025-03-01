5a9b793f40378ff30f1116efee9d595fe8886c0f
mchung25 <102553221+mchung25@users.noreply.github.com>
2025-02-28 22:00:45 -0600

    [minhee] ran on eval

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,7 @@
 export MODEL_ID="deepseek-ai/DeepSeek-V3"
-export PROMPT_TYPE="cot"
-export INPUT_DIR="eval/total_goals/persona_and_scenario/"
+export PROMPT_TYPE="response"
+export INPUT_DIR="eval/persona_goal/"
 export INPUT_FILE="test_output_split.json"
-export OUTPUT_DIR="eval/total_goals/persona_and_scenario/"
-export OUTPUT_FILE="test_output_labeled_cot.json"
+export OUTPUT_DIR="eval/persona_goal/"
+export OUTPUT_FILE="test_output_labeled_response.json"
 python src/auto_eval_harmful.py --model_id $MODEL_ID --prompt_type $PROMPT_TYPE --input_path $INPUT_DIR/$INPUT_FILE --output_path $OUTPUT_DIR/$OUTPUT_FILE
